ef2bc28863229f9ccb62dbbe02786acfd95830a7
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-06-11 20:33:48 +0530

    -refactoring...

--- lexical/lexi.c
+++ lexical/lexi.c
@@ -295,10 +295,10 @@ void lexical_analyzer(const char *file_name) {
 
         char buffer[1024];
         while (fgets(buffer, sizeof(buffer), file) != NULL) {
-                //  //parse_keywords(buffer);
-                //  parse_operators(buffer);
-                //  parse_punctuators(buffer);
-                //  parse_identifiers(buffer);
+                parse_keywords(buffer);
+                parse_operators(buffer);
+                parse_punctuators(buffer);
+                parse_identifiers(buffer);
                 parse_literals(buffer);
         }
         rewind(file);
